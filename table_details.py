f9925332ece4968d1480f8f6738b786e84007159
KrushnaBaral <krushna.baral@lagozon.com>
2025-12-16 14:41:13 +0530

    Update table_details.py

--- table_details.py
+++ table_details.py
@@ -24,9 +24,9 @@ llm = AzureChatOpenAI(
 )
 
 from typing import List
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 def get_table_details(table_name=None):
     """
     Returns details for one or more tables from hardcoded JSON files.
@@ -166,3 +166,4 @@ def get_table_metadata(json_filename='expanded_tables.json'):
 # mock_question_test = "How many product view by products in last week"
 # table_chain_check = table_chain.invoke({"question":mock_question_test})
 # print("test table chain  first mock_question  :" , mock_question_test ,"  Now tables selected:... ",table_chain_check)
+
